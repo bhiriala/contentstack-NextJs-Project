1f53c88c3d0fa2341b67e5dc3c49b83bd4345c52
Ala Ghoul Bhiri <bhiri.a@decade.fr>
2025-09-10 18:08:31 +0200

    Initial commit

---
+++ lib/contentstack.ts
@@ -0,0 +1,58 @@
+import contentstack, { QueryOperation } from "@contentstack/delivery-sdk";
+import ContentstackLivePreview, { IStackSdk } from "@contentstack/live-preview-utils";
+import { Page } from "./types";
+import { getContentstackEndpoints, getRegionForString } from "@timbenniks/contentstack-endpoints";
+
+const region = getRegionForString(process.env.NEXT_PUBLIC_CONTENTSTACK_REGION as string)
+const endpoints = getContentstackEndpoints(region, true)
+
+export const stack = contentstack.stack({
+  apiKey: process.env.NEXT_PUBLIC_CONTENTSTACK_API_KEY as string,
+  deliveryToken: process.env.NEXT_PUBLIC_CONTENTSTACK_DELIVERY_TOKEN as string,
+  environment: process.env.NEXT_PUBLIC_CONTENTSTACK_ENVIRONMENT as string,
+  region: region ? region : process.env.NEXT_PUBLIC_CONTENTSTACK_REGION as any,
+  host: process.env.NEXT_PUBLIC_CONTENTSTACK_CONTENT_DELIVERY || endpoints && endpoints.contentDelivery,
+
+  live_preview: {
+    enable: process.env.NEXT_PUBLIC_CONTENTSTACK_PREVIEW === 'true',
+    preview_token: process.env.NEXT_PUBLIC_CONTENTSTACK_PREVIEW_TOKEN,
+    host: process.env.NEXT_PUBLIC_CONTENTSTACK_PREVIEW_HOST || endpoints && endpoints.preview
+  }
+});
+
+export function initLivePreview() {
+  ContentstackLivePreview.init({
+    ssr: false,
+    enable: process.env.NEXT_PUBLIC_CONTENTSTACK_PREVIEW === 'true', 
+    mode: "builder", 
+    stackSdk: stack.config as IStackSdk, 
+    stackDetails: {
+      apiKey: process.env.NEXT_PUBLIC_CONTENTSTACK_API_KEY as string,
+      environment: process.env.NEXT_PUBLIC_CONTENTSTACK_ENVIRONMENT as string,
+    },
+    clientUrlParams: {
+      host: process.env.NEXT_PUBLIC_CONTENTSTACK_CONTENT_APPLICATION || endpoints && endpoints.application
+    },
+    editButton: {
+      enable: true,
+      exclude: ["outsideLivePreviewPortal"]
+    },
+  });
+}
+export async function getPage(url: string) {
+  const result = await stack
+    .contentType("page")
+    .entry()
+    .query() 
+    .where("url", QueryOperation.EQUALS, url) 
+    .find<Page>(); 
+
+  if (result.entries) {
+    const entry = result.entries[0]; 
+
+    if (process.env.NEXT_PUBLIC_CONTENTSTACK_PREVIEW === 'true') {
+      contentstack.Utils.addEditableTags(entry, 'page', true); 
+    }
+    return entry; 
+  }
+}
